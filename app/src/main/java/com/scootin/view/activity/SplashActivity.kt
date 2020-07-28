d3d6402e24528e14e5647975d4aa3dc7e284d1cf
Khare, Saurabh | Saurabh | TID <saurabh.khare@rakuten.com>
2020-07-28 20:16:34 +0900

    Login screen logic start..

--- app/src/main/java/com/scootin/view/activity/SplashActivity.kt
+++ app/src/main/java/com/scootin/view/activity/SplashActivity.kt
@@ -3,14 +3,19 @@ package com.scootin.view.activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.observe
 import com.scootin.R
 import com.scootin.databinding.ActivitySplashBinding
+import com.scootin.viewmodel.home.SplashViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySplashBinding
+    private val viewModel: SplashViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +26,7 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun setUi() {
-        val video =
-            Uri.parse(
-                "android.resource://" + packageName
-                    .toString() + "/" + R.raw.video
-            )
+        val video = Uri.parse("android.resource://" + packageName.toString() + "/" + R.raw.video)
         binding.videoView.apply {
             setVideoURI(video)
             setOnCompletionListener { startNextActivity() }
@@ -35,8 +36,13 @@ class SplashActivity : AppCompatActivity() {
 
     private fun startNextActivity() {
         if (isFinishing) return
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+
+        viewModel.firstLaunch().observe(this) {
+            Timber.i("First launch.. $it")
+            //Check for first time launch
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 
 }
