2bb904bd62560c732f91f5de683792b249ad67fa
Khare, Saurabh | Saurabh | TID <saurabh.khare@rakuten.com>
2020-08-03 11:41:49 +0900

    fragment added.

--- app/src/main/java/com/scootin/view/fragment/dialogs/CategoryDialogFragment.kt
+++ app/src/main/java/com/scootin/view/fragment/dialogs/CategoryDialogFragment.kt
@@ -1,20 +1,36 @@
 package com.scootin.view.fragment.dialogs
 
+import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import com.scootin.R
+import timber.log.Timber
 
 
 class CategoryDialogFragment: DialogFragment() {
 
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        return inflater.inflate(R.layout.dialog_lottie, container, false)
-//    }
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+
+            builder.setTitle("Select Cetegory").setSingleChoiceItems(getListItem(), 0
+            ) { dialog, which -> Timber.i("Checked = $which") }
+                .setPositiveButton(android.R.string.ok, { dialog, id ->
+                    Timber.i("positive = $id")
+                })
+            .setNegativeButton(android.R.string.cancel, { dialog, id ->
+                Timber.i("negative = $id")
+                })
+
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
+    }
+
+    private fun getListItem() = requireContext().resources.getStringArray(R.array.express_delivery_options)
+
 }
