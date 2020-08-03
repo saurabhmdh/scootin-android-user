74df71ba7bf75f75c52a4a7647be81ce274511b1
Khare, Saurabh | Saurabh | TID <saurabh.khare@rakuten.com>
2020-08-03 11:50:17 +0900

    added working on cetegory dialog

--- app/src/main/java/com/scootin/view/fragment/dialogs/CategoryDialogFragment.kt
+++ app/src/main/java/com/scootin/view/fragment/dialogs/CategoryDialogFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.findNavController
 import com.scootin.R
 import timber.log.Timber
 
@@ -18,14 +19,14 @@ class CategoryDialogFragment: DialogFragment() {
         return activity?.let {
             val builder = AlertDialog.Builder(it)
 
-            builder.setTitle("Select Cetegory").setSingleChoiceItems(getListItem(), 0
-            ) { dialog, which -> Timber.i("Checked = $which") }
-                .setPositiveButton(android.R.string.ok, { dialog, id ->
-                    Timber.i("positive = $id")
-                })
-            .setNegativeButton(android.R.string.cancel, { dialog, id ->
-                Timber.i("negative = $id")
-                })
+            builder.setTitle("Select Cetegory").setSingleChoiceItems(getListItem(), 0) { dialog, which ->
+                Timber.i("Checked = $which")
+            }.setPositiveButton(android.R.string.ok) { dialog, id ->
+                Timber.i("positive = $id")
+            }.setNegativeButton(android.R.string.cancel) { dialog, id ->
+                Timber.i("user didn't select any option = $id")
+                findNavController().popBackStack()
+            }
 
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
