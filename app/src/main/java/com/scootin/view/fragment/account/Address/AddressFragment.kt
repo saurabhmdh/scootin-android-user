d6360846a35a7338f96bd63aaededc8172406a14
EdawKin <68758883+EdawKin@users.noreply.github.com>
2020-09-08 21:14:29 +0530

    I have made the changes, please have a look. I have added the ViewModel classes.

---
+++ app/src/main/java/com/scootin/view/fragment/account/Address/AddressFragment.kt
@@ -0,0 +1,32 @@
+package com.scootin.view.fragment.account.Address
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.scootin.R
+import com.scootin.databinding.FragmentAccountBinding
+import com.scootin.network.AppExecutors
+import com.scootin.util.fragment.autoCleared
+import com.scootin.viewmodel.account.AccountFragmentViewModel
+import com.scootin.viewmodel.account.AddressFragmentViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class AddressFragment : Fragment(R.layout.fragment_add_new_address) {
+
+    private var binding by autoCleared<FragmentAccountBinding>()
+    private val viewModel: AddressFragmentViewModel by viewModels()
+
+    @Inject
+    lateinit var appExecutors: AppExecutors
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentAccountBinding.bind(view)
+
+    }
+}
---
+++ app/src/main/java/com/scootin/view/fragment/account/Support/SupportFragment.kt
@@ -0,0 +1,31 @@
+package com.scootin.view.fragment.account.Support
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.scootin.R
+import com.scootin.databinding.FragmentAccountBinding
+import com.scootin.network.AppExecutors
+import com.scootin.util.fragment.autoCleared
+import com.scootin.viewmodel.account.AccountFragmentViewModel
+import com.scootin.viewmodel.account.SupportFragmentViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class SupportFragment : Fragment(R.layout.fragment_customer_support) {
+
+    private var binding by autoCleared<FragmentAccountBinding>()
+    private val viewModel: SupportFragmentViewModel by viewModels()
+
+    @Inject
+    lateinit var appExecutors: AppExecutors
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentAccountBinding.bind(view)
+
+    }
+
+}
