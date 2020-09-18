38db057feec701075b74be8612e3b207df47dfc9
Saurabh Khare <saurabhmdh@gmail.com>
2020-09-19 07:54:42 +0900

    some version has been updated

--- app/src/main/java/com/scootin/network/api/APIService.kt
+++ app/src/main/java/com/scootin/network/api/APIService.kt
@@ -20,4 +20,6 @@ interface APIService {
 
     @POST("auth/generate-otp")
     suspend fun requestOTP(@Body options: Map<String, String>, @HeaderMap map: Map<String, String> = AppHeaders.getHeaderMap()) : Response<ResponseBody>
+
+
 }
