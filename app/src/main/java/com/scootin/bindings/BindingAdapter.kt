13856ab4653fc0048457756826dcf35763290fbd
Saurabh Khare <saurabhmdh@gmail.com>
2020-10-19 21:27:09 +0900

    Price added properly

--- app/src/main/java/com/scootin/bindings/BindingAdapter.kt
+++ app/src/main/java/com/scootin/bindings/BindingAdapter.kt
@@ -2,11 +2,16 @@ package com.scootin.bindings
 
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
 import com.scootin.R
 import com.scootin.network.glide.GlideApp
+import java.math.BigDecimal
+import java.text.Format
+import java.text.NumberFormat
+import java.util.*
 
 @BindingAdapter("visibleGone")
 fun View.visibleGone(show: Boolean) {
@@ -24,4 +29,12 @@ fun ImageView.setImage(url: String) {
 fun getDefaultImage() = RequestOptions().apply {
     placeholder(R.drawable.ic_placeholder)
     error(R.drawable.ic_placeholder)
+}
+
+@BindingAdapter("setPrice")
+fun TextView.setPrice(value: Double) {
+    val format: Format = NumberFormat.getCurrencyInstance(Locale("en", "in"))
+    val finalValue =  format.format(BigDecimal(value))
+
+    text = finalValue
 }
