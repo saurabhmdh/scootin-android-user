993f499dfffbcbd4c39b84fcb093174dc657dafc
Saurabh Khare <saurabhmdh@gmail.com>
2020-11-01 02:55:11 +0900

    small code done for express delivery

--- app/src/main/java/com/scootin/view/fragment/delivery/express/ExpressDeliveryCategoryFragment.kt
+++ app/src/main/java/com/scootin/view/fragment/delivery/express/ExpressDeliveryCategoryFragment.kt
@@ -2,22 +2,25 @@ package com.scootin.view.fragment.delivery.express
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import android.widget.LinearLayout
+import android.widget.RadioButton
+import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
 import com.scootin.R
+import com.scootin.database.table.Cache
 import com.scootin.databinding.FragmentExpressDeliveryBinding
+import com.scootin.extensions.getCheckedRadioButtonPosition
 import com.scootin.network.AppExecutors
 import com.scootin.network.api.Status
 import com.scootin.network.response.home.HomeResponseCategory
-import com.scootin.network.response.sweets.SweetsStore
+import com.scootin.util.constants.AppConstants
 import com.scootin.util.fragment.autoCleared
-import com.scootin.view.adapter.EssentialGroceryStoreAdapter
-import com.scootin.view.adapter.ExpressCategoryAdapter
 import com.scootin.viewmodel.home.HomeFragmentViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -25,62 +28,30 @@ import javax.inject.Inject
 class ExpressDeliveryCategoryFragment : Fragment(R.layout.fragment_express_delivery) {
     private var binding by autoCleared<FragmentExpressDeliveryBinding>()
 
-    private val viewModel: HomeFragmentViewModel by viewModels()
-
-    var mSelectPos=-1
-    private lateinit var homeCategoryList: List<HomeResponseCategory>
-
     @Inject
     lateinit var appExecutors: AppExecutors
-    private lateinit var expressDeliveryAdapter: ExpressCategoryAdapter
+    private val viewModel: HomeFragmentViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentExpressDeliveryBinding.bind(view)
         setupListener()
-
-        setStoreAdapter()
-        doNetworkCall()
     }
 
     private fun setupListener() {
         binding.back.setOnClickListener { findNavController().popBackStack() }
-    }
 
+        binding.btnDone.setOnClickListener {
+            val position = binding.radioGroup.getCheckedRadioButtonPosition()
+            val selectedView = binding.radioGroup.get(position)
 
-    private fun setStoreAdapter() {
-        expressDeliveryAdapter = ExpressCategoryAdapter(
-            appExecutors,ExpressDeliveryCategoryFragment(),
-            object : ExpressCategoryAdapter.StoreImageAdapterClickListener {
+            Timber.i("selected view ${selectedView.tag}")
+            viewModel.updateMainCategory(it.tag as String?)
 
-                override fun onSelectButtonSelected(position: Int) {
-                    mSelectPos=position
-                    expressDeliveryAdapter.notifyDataSetChanged()
-                }
-
-            })
-        binding.list.apply {
-            adapter = expressDeliveryAdapter
+            //Move to next screen
         }
     }
 
 
-    private fun doNetworkCall() {
-        viewModel.getHomeCategory().observe(viewLifecycleOwner, {
-            when (it.status) {
-                Status.ERROR -> {
-                }
-                Status.SUCCESS -> {
-                    Timber.i("Category Response ${it.data}")
-                    it.data?.let {list->
-                        homeCategoryList = list
-                        expressDeliveryAdapter.submitList(homeCategoryList)
-                    }
-                }
-                Status.LOADING -> {
-                }
-            }
-        })
-    }
 }
 
