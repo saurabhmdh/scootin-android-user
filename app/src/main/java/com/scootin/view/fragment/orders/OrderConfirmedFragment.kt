099372d87b66e3136d28f6761f1f93faef2123e2
alok Mishra <alok.mishra@rakuten.com>
2020-11-01 22:50:00 +0900

    order confirm success n placed

---
+++ app/src/main/java/com/scootin/view/fragment/orders/OrderConfirmedFragment.kt
@@ -0,0 +1,8 @@
+package com.scootin.view.fragment.orders
+
+import androidx.fragment.app.Fragment
+import com.scootin.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class OrderConfirmedFragment : Fragment(R.layout.fragment_order_confirmed)
