3a7aa51ddd4337c12593a42fd2306e6a4b893de9
Saurabh Khare <saurabhmdh@gmail.com>
2020-11-02 16:27:40 +0900

    working on select media

--- app/src/main/java/com/scootin/view/fragment/delivery/essential/EssentialHandwrittenFragment.kt
+++ app/src/main/java/com/scootin/view/fragment/delivery/essential/EssentialHandwrittenFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.scootin.R
@@ -18,7 +17,6 @@ import com.scootin.network.manager.AppHeaders
 import com.scootin.network.request.DirectOrderRequest
 import com.scootin.util.constants.AppConstants
 import com.scootin.util.fragment.autoCleared
-import com.scootin.util.ui.FileUtils
 import com.scootin.util.ui.MediaPicker
 import com.scootin.util.ui.UtilPermission
 import com.scootin.viewmodel.order.DirectOrderViewModel
@@ -48,13 +46,6 @@ class EssentialHandwrittenFragment : Fragment(R.layout.hand_written_grocery_list
             onClickOfUploadMedia()
         }
 
-        viewModel.filePathLiveData.observe(viewLifecycleOwner, { data->
-            Timber.i("EssentialHandwrittenFragment response = ${data.isSuccessful}")
-            if (data.isSuccessful) {
-                val media = data.body()
-                //mediaId = media?.id ?: -1L
-            }
-        })
         binding.back.setOnClickListener { findNavController().popBackStack() }
 
         binding.placeOrder.setOnClickListener {
@@ -96,11 +87,16 @@ class EssentialHandwrittenFragment : Fragment(R.layout.hand_written_grocery_list
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
         if (requestCode == AppConstants.RESULT_LOAD_IMAGE_VIDEO && resultCode == Activity.RESULT_OK && null != intent) {
-            context?.let { context ->
-                GlideApp.with(requireContext()).load(intent.data).into(binding.receiverPhotoBox)
-                intent.data?.let {
-                    viewModel.filePath(it)
-
+            //Call view model to upload media..
+            intent.data?.let { uri ->
+                viewModel.uploadMedia(uri).observe(viewLifecycleOwner) {response->
+                    if(response.isSuccessful) {
+                        val media = response.body() ?: return@observe
+                        GlideApp.with(requireContext()).load(media.url).into(binding.receiverPhotoBox)
+                        mediaId = media.id
+                    } else {
+                        Toast.makeText(context, "There is some error media", Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
         }
