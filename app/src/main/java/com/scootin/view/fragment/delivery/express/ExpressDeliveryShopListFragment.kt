863b1e0c555de9aa49c7f3a406016b2cd77189fe
gsrathoreniks <niks.gajsa@gmail.com>
2020-11-02 11:44:55 +0530

    Shop List fragment added

---
+++ app/src/main/java/com/scootin/view/fragment/delivery/express/ExpressDeliveryShopListFragment.kt
@@ -0,0 +1,110 @@
+package com.scootin.view.fragment.delivery.express
+
+import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import com.scootin.R
+import com.scootin.databinding.FragmentExpressDeliveryShoplistBinding
+import com.scootin.databinding.FragmentGroceryDeliveryBinding
+import com.scootin.extensions.getCheckedRadioButtonPosition
+import com.scootin.extensions.updateVisibility
+import com.scootin.network.AppExecutors
+import com.scootin.network.manager.AppHeaders
+import com.scootin.network.request.AddToCartRequest
+import com.scootin.network.response.SearchProductsByCategoryResponse
+import com.scootin.network.response.SearchShopsByCategoryResponse
+import com.scootin.util.fragment.autoCleared
+import com.scootin.view.adapter.ProductSearchAdapter
+import com.scootin.view.adapter.ShopSearchAdapter
+import com.scootin.viewmodel.delivery.CategoriesViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
+import javax.inject.Inject
+
+
+@AndroidEntryPoint
+class ExpressDeliveryShopListFragment : Fragment(R.layout.fragment_express_delivery_shoplist) {
+    private var binding by autoCleared<FragmentExpressDeliveryShoplistBinding>()
+    private val viewModel: CategoriesViewModel by viewModels()
+
+    @Inject
+    lateinit var appExecutors: AppExecutors
+    private lateinit var productSearchAdapter: ProductSearchAdapter
+    private lateinit var shopSearchAdapter: ShopSearchAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentExpressDeliveryShoplistBinding.bind(view)
+        updateUI()
+        updateListeners()
+
+        viewModel.addToCartMap.observe(viewLifecycleOwner, Observer {
+            Timber.i("Status addToCartLiveData = ${it.isSuccessful} ")
+        })
+    }
+
+    private fun updateUI() {
+        setStoreAdapter()
+//        setProductAdapter()
+    }
+
+    private fun updateListeners() {
+        //When the screen load lets load the data for empty screen
+        viewModel.doSearch("")
+
+        binding.searchBox.setOnQueryTextListener(
+            object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    if (query != null) {
+                        viewModel.doSearch(query)
+                        Timber.i("onQueryTextSubmit $query ")
+                    }else{
+                        Toast.makeText(context,"Enter text to search",Toast.LENGTH_SHORT).show()
+                    }
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    return false
+                }
+
+            }
+        )
+        binding.back.setOnClickListener { findNavController().popBackStack() }
+
+        viewModel.shops.observe(viewLifecycleOwner) {
+            if (it.isSuccessful) {
+                shopSearchAdapter.submitList(it.body())
+            }
+            Timber.i("Search result for shop ${it.body()}")
+        }
+
+        viewModel.product.observe(viewLifecycleOwner) {
+            if (it.isSuccessful) {
+                productSearchAdapter.submitList(it.body())
+            }
+        }
+    }
+
+
+    private fun setStoreAdapter() {
+        shopSearchAdapter = ShopSearchAdapter(
+            appExecutors, object : ShopSearchAdapter.StoreImageAdapterClickListener {
+                override fun onSelectButtonSelected(shopInfo: SearchShopsByCategoryResponse) {
+                    Timber.i("Shop Info $shopInfo")
+                    viewModel.updateShop(shopInfo.shopID)
+                    binding.storeList.updateVisibility(false)
+                }
+            })
+        binding.storeList.apply {
+            adapter = shopSearchAdapter
+        }
+    }
+}
+
