60c7469ec289d78cc75f64a3539a94b45c30561d
Saurabh Khare <saurabhmdh@gmail.com>
2020-11-03 21:45:48 +0900

    updated network response

--- app/src/main/java/com/scootin/network/response/orderdetail/PaymentDetails.kt
+++ app/src/main/java/com/scootin/network/response/orderdetail/PaymentDetails.kt
@@ -2,6 +2,10 @@ package com.scootin.network.response.orderdetail
 
 data class PaymentDetails(
     val deliveryFreeAmount: Double,
-    val id: Int,
-    val payment_status: String
+    val amount: Double,
+    val totalGSTAmount: Double,
+    val totalAmount: Double,
+    val id: Long,
+    val paymentStatus: String,
+    val paymentMode: String?
 )
