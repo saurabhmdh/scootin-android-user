213c27ffa0f9062930f8f0ec6e7409dd00eced18
Saurabh Khare <saurabhmdh@gmail.com>
2020-11-24 15:05:10 +0900

    fragment should be finish after 15s

--- app/src/main/java/com/scootin/view/fragment/orders/SuccessFragment.kt
+++ app/src/main/java/com/scootin/view/fragment/orders/SuccessFragment.kt
@@ -1,8 +1,20 @@
 package com.scootin.view.fragment.orders
 
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.scootin.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SuccessFragment : Fragment(R.layout.fragment_success)
+class SuccessFragment : Fragment(R.layout.fragment_success) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler().postDelayed({
+            findNavController().popBackStack()
+        }, 1500)
+    }
+}
