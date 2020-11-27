1798553326002bf7e31778833b50f579ba831e75
Saurabh Khare <saurabhmdh@gmail.com>
2020-11-27 19:11:02 +0900

    add address code..

--- app/src/main/java/com/scootin/view/fragment/cart/CardPaymentPageFragment.kt
+++ app/src/main/java/com/scootin/view/fragment/cart/CardPaymentPageFragment.kt
@@ -9,10 +9,13 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 
 import androidx.navigation.fragment.navArgs
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.razorpay.Checkout
 import com.scootin.R
 import com.scootin.databinding.FragmentPaymenttStatusBinding
 import com.scootin.extensions.getCheckedRadioButtonPosition
+import com.scootin.extensions.getNavigationResult
 import com.scootin.extensions.orDefault
 import com.scootin.network.AppExecutors
 import com.scootin.network.api.Status
@@ -20,6 +23,7 @@ import com.scootin.network.manager.AppHeaders
 import com.scootin.network.request.OrderRequest
 import com.scootin.network.request.VerifyAmountRequest
 import com.scootin.network.response.AddressDetails
+import com.scootin.util.UtilUIComponent
 import com.scootin.util.constants.AppConstants
 import com.scootin.util.constants.IntentConstants
 import com.scootin.util.fragment.autoCleared
@@ -83,11 +87,17 @@ class CardPaymentPageFragment : BaseFragment(R.layout.fragment_paymentt_status) 
         viewModel.loadAllAddress().observe(viewLifecycleOwner) {
             //find defaultAddress..
             if (it.isSuccessful) {
+
+                if (address != null) {
+                    Timber.i("We have address from previous fragment $address")
+                    return@observe
+                }
                 address = it.body()?.first { it.hasDefault }
                 Timber.i("We found address ${address}")
-                if (address == null) {
-
+                address?.let {
+                    binding.editDropAddress.text = UtilUIComponent.setOneLineAddress(address)
                 }
+
             } else {
                 //We need to
             }
@@ -95,32 +105,36 @@ class CardPaymentPageFragment : BaseFragment(R.layout.fragment_paymentt_status) 
 
 
         binding.confirmButton.setOnClickListener {
+            if (address == null) {
+                Toast.makeText(requireContext(), "Please add a address", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
             val mode = when(binding.radioGroup.getCheckedRadioButtonPosition()) {
                 0 -> {"ONLINE"}
                 1 -> {"CASH"}
                 else -> {""}
             }
             showLoading()
-            //Timber.i
-//            viewModel.userConfirmOrder(orderId.toString(), AppHeaders.userID, OrderRequest(mode, AppConstants.defaultAddressId)).observe(viewLifecycleOwner) {
-//                when(it.status) {
-//                    Status.SUCCESS -> {
-//
-//                        Timber.i(" data ${it.data}")
-//                        dismissLoading()
-//                        if (it.data?.paymentDetails?.paymentMode.equals("ONLINE")) {
-//                            val total = it.data?.paymentDetails?.totalAmount.orDefault(0.0) * 100
-//                            startPayment(it.data?.paymentDetails?.orderReference.orEmpty(), total)
-//                        } else {
-//                            findNavController().navigate(CardPaymentPageFragmentDirections.orderConfirmationPage(orderId))
-//                        }
-//                    }
-//                    Status.ERROR -> {
-//                        dismissLoading()
-//                    }
-//                    Status.LOADING -> {}
-//                }
-//            }
+
+            viewModel.userConfirmOrder(orderId.toString(), AppHeaders.userID, OrderRequest(mode, address!!.id)).observe(viewLifecycleOwner) {
+                when(it.status) {
+                    Status.SUCCESS -> {
+
+                        Timber.i(" data ${it.data}")
+                        dismissLoading()
+                        if (it.data?.paymentDetails?.paymentMode.equals("ONLINE")) {
+                            val total = it.data?.paymentDetails?.totalAmount.orDefault(0.0) * 100
+                            startPayment(it.data?.paymentDetails?.orderReference.orEmpty(), total)
+                        } else {
+                            findNavController().navigate(CardPaymentPageFragmentDirections.orderConfirmationPage(orderId))
+                        }
+                    }
+                    Status.ERROR -> {
+                        dismissLoading()
+                    }
+                    Status.LOADING -> {}
+                }
+            }
         }
 
         binding.applyPromoButton.setOnClickListener {
@@ -146,8 +160,18 @@ class CardPaymentPageFragment : BaseFragment(R.layout.fragment_paymentt_status) 
         binding.editDropAddress.setOnClickListener {
             findNavController().navigate(IntentConstants.openAddressPage())
         }
-    }
 
+        getNavigationResult()?.observe(viewLifecycleOwner) {
+            updateAddressData(it)
+        }
+    }
+    private fun updateAddressData(calendarData: String) {
+        val result =
+            Gson().fromJson<AddressDetails>(calendarData, object : TypeToken<AddressDetails>() {}.type)
+        address = result
+        binding.editDropAddress.text = UtilUIComponent.setOneLineAddress(address)
+        Timber.i("update the address $result")
+    }
 
 
     private fun startPayment(orderReferenceId: String, price: Double) {
