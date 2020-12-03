ea1d1a16b75202a18a00e178148e09c98b304922
DESKTOP-0ARHB49\samridhi <samridhi102kri@gmail.com>
2020-12-03 15:41:33 +0530

    no message

---
+++ app/src/main/java/com/scootin/view/fragment/delivery/essential/EssentialGroceryShopItemListFragment.kt
@@ -0,0 +1,132 @@
+package com.scootin.view.fragment.delivery.essential
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+import com.scootin.R
+import com.scootin.databinding.FragmentEssentialShopItemListBinding
+import com.scootin.databinding.FragmentVegetableDeliveryBinding
+import com.scootin.extensions.orZero
+import com.scootin.network.AppExecutors
+import com.scootin.network.manager.AppHeaders
+import com.scootin.network.request.AddToCartRequest
+import com.scootin.util.fragment.autoCleared
+import com.scootin.view.adapter.ProductSearchPagingAdapter
+import com.scootin.view.vo.ProductSearchVO
+import com.scootin.viewmodel.delivery.CategoriesViewModel
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
+
+class EssentialGroceryShopItemListFragment: Fragment(R.layout.fragment_essential_shop_item_list) {
+    private var binding by autoCleared<FragmentEssentialShopItemListBinding>()
+    private val viewModel: CategoriesViewModel by viewModels()
+
+    @Inject
+    lateinit var appExecutors: AppExecutors
+
+    private var productSearchAdapter by autoCleared<ProductSearchPagingAdapter>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentEssentialShopItemListBinding.bind(view)
+        updateUI()
+        updateListeners()
+        viewModel.loadCount()
+    }
+    private fun updateUI() {
+        setProductAdapter()
+    }
+
+    private fun setProductAdapter() {
+        productSearchAdapter = ProductSearchPagingAdapter(
+            object : ProductSearchPagingAdapter.ImageAdapterClickLister {
+                override fun onIncrementItem(
+                    view: View,
+                    item: ProductSearchVO?,
+                    count: Int
+                ) {
+                    val addToCartRequest = AddToCartRequest(AppHeaders.userID.toInt(), item?.id, count)
+                    viewModel.addToCart(addToCartRequest)
+                }
+
+                override fun onDecrementItem(
+                    view: View,
+                    item: ProductSearchVO?,
+                    count: Int
+                ) {
+                    val addToCartRequest = AddToCartRequest(AppHeaders.userID.toInt(), item?.id, count)
+                    viewModel.addToCart(addToCartRequest)
+                }
+
+            })
+
+        binding.productList.apply {
+            adapter = productSearchAdapter
+        }
+    }
+
+    private fun updateListeners() {
+        //When the screen load lets load the data for empty screen
+        viewModel.doSearch("")
+
+        binding.searchBox.setOnQueryTextListener(
+            object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    query?.let {
+                        viewModel.doSearch(it)
+                    }
+                    Timber.i("onQueryTextSubmit $query ")
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    if (newText.isNullOrEmpty()) {
+                        viewModel.doSearch("")
+                    }
+                    return false
+                }
+
+            }
+        )
+        binding.back.setOnClickListener { findNavController().popBackStack() }
+
+        viewModel.allProduct.observe(viewLifecycleOwner) {response->
+            lifecycleScope.launch {
+                productSearchAdapter.submitData(response)
+            }
+        }
+
+        binding.fabCart.setOnClickListener {
+            val navOptions =
+                NavOptions.Builder().setPopUpTo(R.id.titleScreen, false).build()
+            findNavController().navigate(R.id.cart, null, navOptions)
+        }
+
+        viewModel.getCartCount.observe(viewLifecycleOwner) {
+            if (it.isSuccessful) {
+                val result = it.body()?.toInt().orZero()
+                setupBadge(result)
+            }
+        }
+        viewModel.addToCartMap.observe(viewLifecycleOwner, {
+            Timber.i("Status addToCartLiveData = ${it?.isSuccessful} ")
+            viewModel.loadCount()
+        })
+    }
+
+    private fun setupBadge(result: Int) {
+        if (result == 0) {
+            binding.textCount.visibility = View.GONE
+        } else {
+            binding.textCount.visibility = View.VISIBLE
+            binding.textCount.text = result.toString()
+        }
+    }
+
+}
