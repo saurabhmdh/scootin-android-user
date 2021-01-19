a03fb689fe186da3749667e4406ab13cdb3c741c
Saurabh Khare <saurabhmdh@gmail.com>
2021-01-19 14:35:12 +0900

    clear cache when user re-launch application

--- app/src/main/java/com/scootin/services/StartupDataBaseWorker.kt
+++ app/src/main/java/com/scootin/services/StartupDataBaseWorker.kt
@@ -5,9 +5,11 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.google.android.libraries.places.api.Places
 import com.scootin.R
+import com.scootin.database.dao.CacheDao
 import com.scootin.database.dao.LocationDao
 import com.scootin.di.IWorkerFactory
 import com.scootin.network.AppExecutors
+import com.scootin.util.constants.AppConstants
 import kotlinx.coroutines.coroutineScope
 import timber.log.Timber
 import javax.inject.Inject
@@ -16,12 +18,16 @@ class StartupDataBaseWorker (
     private val context: Application,
     params: WorkerParameters,
     val locationDao: LocationDao,
+    val cacheDao: CacheDao
 ): CoroutineWorker(context, params) {
     override suspend fun doWork(): Result = coroutineScope {
-        Timber.i("Running Successfully..")
         //Lets delete old location data
         locationDao.clearAll()
+        //Clear some cache data
+        cacheDao.deleteCache(AppConstants.FCM_ID)
         Places.initialize(context, context.getString(R.string.google_maps_key))
+
+        Timber.i("Running Successfully..")
         Result.success()
     }
 
@@ -30,11 +36,12 @@ class StartupDataBaseWorker (
     class Factory @Inject constructor(
         val appExecutors: AppExecutors,
         val locationDao: LocationDao,
-        val application: Application)
-        : IWorkerFactory<StartupDataBaseWorker> {
+        val application: Application,
+        val cacheDao: CacheDao
+    ) : IWorkerFactory<StartupDataBaseWorker> {
 
         override fun create(params: WorkerParameters): StartupDataBaseWorker {
-            return StartupDataBaseWorker(application, params, locationDao)
+            return StartupDataBaseWorker(application, params, locationDao, cacheDao)
         }
     }
 }
