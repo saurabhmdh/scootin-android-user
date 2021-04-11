6287a2a3f763f17e2f2f08e508c9cff76b452167
Khare, Saurabh | Saurabh | TID <saurabh.khare@rakuten.com>
2021-04-11 15:19:55 +0900

    Updated support for Jio numbers

--- app/src/main/java/com/scootin/util/constants/Validation.kt
+++ app/src/main/java/com/scootin/util/constants/Validation.kt
@@ -5,7 +5,7 @@ import java.util.regex.Pattern
 object Validation {
 
     val REGEX_VALID_OTP = Pattern.compile("\\d{6}")
-    val REGEX_VALID_MOBILE_NUMBER = Pattern.compile("^[897][0-9]{9}")
+    val REGEX_VALID_MOBILE_NUMBER = Pattern.compile("^[6789][0-9]{9}")
 
     val REGEX_VALID_EMAIL = Pattern.compile("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+\$)")
 }
