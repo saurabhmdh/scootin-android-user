6ed9d6e54cab03cc45947da59dc017531d4c9058
Khare, Saurabh | Saurabh | TID <saurabh.khare@rakuten.com>
2021-04-28 17:11:24 +0900

    Updated shop with category & sub category

--- app/src/main/java/com/scootin/network/request/RequestSearchWithCategoryAndSubCategory.kt
+++ app/src/main/java/com/scootin/network/request/RequestSearchWithCategoryAndSubCategory.kt
@@ -1,8 +1,6 @@
 package com.scootin.network.request
 
 data class RequestSearchWithCategoryAndSubCategory(
-    val latitude: Double = 0.0,
-    val longitude: Double = 0.0,
     val query: String = "",
     val categoryId: Long = -1,
     val subCategoryId: Long = -1
