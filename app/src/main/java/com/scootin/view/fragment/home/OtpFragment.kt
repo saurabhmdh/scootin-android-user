12a1027e8760dc5891229b7944a03e6b33b85d5d
Khare, Saurabh | Saurabh | TID <saurabh.khare@rakuten.com>
2021-07-21 04:30:48 +0900

    OTP was sending twice.

--- app/src/main/java/com/scootin/view/fragment/home/OtpFragment.kt
+++ app/src/main/java/com/scootin/view/fragment/home/OtpFragment.kt
@@ -1,28 +1,19 @@
 package com.scootin.view.fragment.home
 
-import android.graphics.Color
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.text.SpannableString
-import android.text.Spanned
-import android.text.TextPaint
-import android.text.method.LinkMovementMethod
-import android.text.style.ClickableSpan
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.scootin.R
-import com.scootin.databinding.FragmentLoginBinding
 import com.scootin.databinding.FragmentSendOtpBinding
 import com.scootin.network.api.Status
 import com.scootin.network.manager.AppHeaders
-import com.scootin.util.constants.AppConstants
 import com.scootin.util.constants.Validation
 import com.scootin.util.fragment.autoCleared
 import com.scootin.view.fragment.BaseFragment
-import com.scootin.view.fragment.delivery.express.ExpressDeliveryOrdersArgs
 import com.scootin.viewmodel.login.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
@@ -39,11 +30,10 @@ class OtpFragment: BaseFragment(R.layout.fragment_send_otp) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSendOtpBinding.bind(view)
-        binding.mobile.setText(mobile)
-
+        binding.mobile.text = mobile
         binding.txtResendOtp.isClickable=false
         timer.start()
-        viewModel.sendOTP(mobile)
+
         binding.back.setOnClickListener {
             findNavController().popBackStack()
         }
