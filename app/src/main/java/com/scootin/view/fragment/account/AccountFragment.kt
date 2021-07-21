6e246002675b23fde98751c9cb9d6226ccd7186a
Khare, Saurabh | Saurabh | TID <saurabh.khare@rakuten.com>
2021-07-21 12:13:26 +0900

    Account screen modified.

--- app/src/main/java/com/scootin/view/fragment/account/AccountFragment.kt
+++ app/src/main/java/com/scootin/view/fragment/account/AccountFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.scootin.BuildConfig
 import com.scootin.R
 import com.scootin.databinding.FragmentAccountBinding
 import com.scootin.network.AppExecutors
@@ -16,6 +17,7 @@ import com.scootin.util.fragment.autoCleared
 import com.scootin.view.activity.SplashActivity
 import com.scootin.viewmodel.account.AccountFragmentViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -49,12 +51,27 @@ class AccountFragment : Fragment(R.layout.fragment_account) {
             when(it.status) {
                 Status.SUCCESS -> {
                     binding.nameEditText.text = it.data?.firstName
+                    val name = it.data?.firstName?.split("\\s+".toRegex())
+                    if (name?.size ?:0 > 1) {
+                        val first = name?.getOrNull(0)?.getOrNull(0)?.toString()
+                        val second = name?.getOrNull(1)?.getOrNull(0)?.toString()
+                        val displayText = first?.capitalize(Locale.ENGLISH) + second?.capitalize(Locale.ENGLISH)
+                        binding.nameInitial.text = displayText
+                    } else {
+                        val first = it.data?.firstName?.getOrNull(0)?.toString()
+                        binding.nameInitial.text = first?.capitalize(Locale.ENGLISH)
+                    }
+
                 }
                 else -> {}
             }
         }
+        binding.versions.text = getVersionDetail()
     }
 
+    private fun getVersionDetail(): String {
+        return "App Version ${BuildConfig.VERSION_CODE} (${BuildConfig.VERSION_NAME})"
+    }
     private fun updateListeners() {
         binding.addressFragment.setOnClickListener {
             findNavController().navigate(AccountFragmentDirections.accountToAddressFragment())
