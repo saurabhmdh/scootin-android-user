dd6660aa3ca78ef2af9bcdcbed43fb43b09658b4
Khare, Saurabh | Saurabh | TID <saurabh.khare@rakuten.com>
2021-07-23 15:07:56 +0900

    Removed un-used files

--- app/src/main/java/com/scootin/view/adapter/DealAdapter.kt
+++ app/src/main/java/com/scootin/view/adapter/DealAdapter.kt
@@ -2,14 +2,10 @@ package com.scootin.view.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
-import com.scootin.databinding.AdapterItemAddCakeDuoBinding
 import com.scootin.databinding.AdapterPromoOffersBinding
 import com.scootin.network.AppExecutors
 import com.scootin.network.response.home.DealResponse
-import com.scootin.network.response.sweets.CakeItem
-import com.scootin.view.holders.DataBoundViewHolder
 
 //class DealAdapter: PagingDataAdapter<DealResponse, DataBoundViewHolder<AdapterPromoOffersBinding>>(diffCallback = object : DiffUtil.ItemCallback<DealResponse>() {
 //    override fun areItemsTheSame(oldItem: DealResponse, newItem: DealResponse): Boolean = oldItem.id == newItem.id
